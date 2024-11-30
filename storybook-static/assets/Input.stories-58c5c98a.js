433f5d0d5ed04a9eb275fd6dd72929c047e3e66e
thomaslein <thomaslein@gmail.com>
2024-11-30 10:06:26 +0100

    Update ignore

--- storybook-static/assets/Input.stories-58c5c98a.js
+++
@@ -1,9 +0,0 @@
-import{j as u}from"./jsx-runtime-50395f49.js";import"./index-9fa1aa67.js";const m="_input_1auw0_1",_={input:m};function e(t){const{className:d,...l}=t;return u.jsx("input",{className:`${d} ${_.input}`,...l})}try{e.displayName="Input",e.__docgenInfo={description:"",displayName:"Input",props:{}}}catch{}const V={title:"Input",component:e,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{placeholder:{control:"text"},onChange:{action:"changed"}}},a={args:{placeholder:"Variant 1"}},r={args:{placeholder:"Variant 2"}};var s,n,o;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    placeholder: "Variant 1"
-  }
-}`,...(o=(n=a.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var c,p,i;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    placeholder: "Variant 2"
-  }
-}`,...(i=(p=r.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};const y=["Variant1","Variant2"];export{a as Variant1,r as Variant2,y as __namedExportsOrder,V as default};
--- storybook-static/assets/Label.stories-3dc52a9d.js
+++
@@ -1,9 +0,0 @@
-import{j as m}from"./jsx-runtime-50395f49.js";import"./index-9fa1aa67.js";const _="_label_mog69_1",u={label:_};function e(s){const{className:p,...d}=s;return m.jsx("label",{className:`${p} ${u.label}`,...d})}try{e.displayName="Label",e.__docgenInfo={description:"",displayName:"Label",props:{}}}catch{}const V={title:"Label",component:e,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{children:{control:"text"}}},a={args:{children:"Variant 1"}},r={args:{children:"Variant 2"}};var t,o,n;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: "Variant 1"
-  }
-}`,...(n=(o=a.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var c,l,i;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    children: "Variant 2"
-  }
-}`,...(i=(l=r.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const y=["Variant1","Variant2"];export{a as Variant1,r as Variant2,y as __namedExportsOrder,V as default};
